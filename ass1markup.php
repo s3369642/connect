71e47c3c4cbee645476b75bacc6bef73e60f0204
Ubuntu <ubuntu@ip-172-31-16-197.ap-southeast-2.compute.internal>
2013-08-23 05:38:53 +0000

    Just done javascript validation

--- ass1markup.php
+++ ass1markup.php
@@ -2,45 +2,60 @@
 <html>
 
 <head>
+
 <?php include 'utility.php'; ?>
 </head>
 
 <body>
+<p><span id="noValuesError" style="color:red; display:none">Please enter one or more field values</span></p>
 
-<form id="wineSearch" method="get">
+<table>
+<col width="30%"
+<tr>
 
-Wine name: <input type="text" id="wineName"><br/>
+<td>
+<form id="wineSearch" method="get" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+
+Wine name: <input type="text" id="wineName" name="wineName">
+<div id="wineError" style="color:red; display:none">Invalid Entry</div>
 <br/>
-Winery name: <input type="text" id="wineryName"><br/>
+<br/>
+Winery name: <input type="text" id="wineryName" name="wineryName">
+<div id="wineryError" style="color:red; display:none">Invalid Entry</div>
+<br/>
 <br/>
 
 Region:<br/>
-<select form="wineSearch" multiple size="10">
+<select form="wineSearch" multiple size="10" id = "region" name="region">
 <?php 
 printDropDown(
 	returnRow("SELECT region_name FROM region;", $dbconn)
 );
 ?>
-</select><br/>
+</select>
 <br/>
+<br/>
+
 Grape Variety:<br/>
-<select form="wineSearch" multiple size="10">
+<select form="wineSearch" multiple size="10" id="grape" name="grape">
 <?php
 printDropDown(
 	returnRow("SELECT DISTINCT variety FROM grape_variety order by variety;", $dbconn)
 );
 ?>
-</select><br/>  
+</select>
+<br/>  
 <br/>
+
 Dating<br/>
 
-From: <select form="wineSearch">
+From: <select form="wineSearch" id="from" name="from">
 <?php
 printDropDown(returnRow("SELECT DISTINCT year FROM wine order by year;", $dbconn));
 ?>
 </select><br/>
 
-To: <select form="wineSearch">
+To: <select form="wineSearch" id="to" name="to">
 <?php
 printDropDown(returnRow("SELECT DISTINCT year FROM wine order by year;", $dbconn));
 ?>
@@ -48,20 +63,39 @@ printDropDown(returnRow("SELECT DISTINCT year FROM wine order by year;", $dbconn
 <br/>
 
 Number in Stock:<br/>
-Min: <input type="number" id="minStock"><br/>
-Max: <input type="number" id="maxStock"><br/>
+Min: <input type="number" id="minStock">
+<div id="minStockError" style="color:red; display:none">Invalid number</div>
+<br/>
+Max: <input type="number" id="maxStock">
+<div id="maxStockError" style="color:red; display:none">Invalid number</div>
+<br/>
 <br/>
 Price Range<br/>
-Min: <input type="number" id="minPrice"><br/>
-Max: <input type="number" id="maxPrice"><br/>
+Min: <input type="number" id="minPrice" name="min">
+<div id="minPriceError" style="color:red; display:none">Invalid number</div>
+<br/>
+Max: <input type="number" id="maxPrice" name="max">
+<div id="maxPriceError" style="color:red; display:none">Invalid number</div>
 <br/>
 
-<input type="text" id="query">
-
-<input type="button" onclick="submitSearch()">
+<!-- flag used by $_GET to tell if the form is submitted-->
+<input type='hidden' id="submitted" name='submitted' value="0" />
+<br/>
+<input type="button" value="Search" onclick="validateSubmit();">
 
 </form>
+</td>
 
+<td>
+<?php
+if(isset($_GET['submitted']) && $_GET['submitted']=="1"){
+	printTable($dbconn);
+	}
+?>
+</td>
+
+</tr>
+</table>
 </body>
 
 </html>
--- utility.php
+++ utility.php
@@ -1,51 +1,240 @@
 <?php
 
-$basePath = dirname(__FILE__);
-include ($basePath."/connect/connect.php");;
+include 'connect.php';
 
-function returnRow($query, $dbconn){
+function returnArray($query, $dbconn){
 	$output = [];
-	$result = mysql_query($query, $dbconn);
-	while($row = mysql_fetch_row($result)){
+	if($result = mysql_query($query, $dbconn)){
+	while($row = mysql_fetch_array($result, MYSQL_ASSOC)){
+		
 		array_push($output, $row);
 		}
 	return $output;
 	}
+	else echo "None found";
+	}
+
+function returnRow($query, $dbconn){
+        $output = [];
+        if($result = mysql_query($query, $dbconn)){
+        while($row = mysql_fetch_row($result)){
+                
+                array_push($output, $row);
+                }
+        return $output;
+        }
+        else echo "None found";
+        }
+
 
 function printDropDown($result){
 	echo "<option value=\"--Select--\" selected>--Select--</option><br/>";
 	foreach($result as $resultRow){
-		echo "<option value=\"$resultRow[0]\">$resultRow[0]</option><br/>";
+		echo "<option value=".$resultRow[0].">".$resultRow[0]."</option><br/>";
 		}
 	}
+
+function printTable($dbconn){
+	$result = returnArray(searchQuery($dbconn), $dbconn);
+
+	if($result != null){
+		echo "<table border \"1\">".
+		"<tr>".
+		"<td>Name</td>".
+		"<td>Year</td>".
+		"<td>Winery</td>".
+		"<td>Variety</td>".
+		"<td>Region</td>".
+		"<td>In Stock</td>".
+		"<td>Cost</td>".
+		"</tr>";
+		foreach($result as $resultRow){
+			echo "<tr>".
+				"<td>".$resultRow['wine_name']."</td>".
+				"<td>".$resultRow['year']."</td>".
+				"<td>".$resultRow['winery_name']."</td>".
+				"<td>".$resultRow['variety']."</td>".
+				"<td>".$resultRow['region_name']."</td>".
+				"<td>".$resultRow['on_hand']."</td>".
+				"<td>$".$resultRow['cost']."</td>".
+				"</tr>";
+		}	
+		echo "</table>";
+	}
+}
+
+
+function searchQuery($dbconn){
+	
+	$query = "select DISTINCT * from wine, winery, region, ".
+		"inventory, ".
+		"wine_variety, grape_variety where ".
+		"wine.winery_id = winery.winery_id and ".
+		"winery.region_id = region.region_id and ".
+		"wine.wine_id = wine_variety.wine_id and ".
+		"grape_variety.variety_id = wine_variety.variety_id and ".
+		"wine.wine_id = inventory.wine_id and ";	
+
+
+	$queryInputs = [];
+
+	if(isset($_GET['wineName']) && $_GET['wineName']!=""){	
+		array_push($queryInputs, "wine_name = \"".$_GET['wineName']."\"");
+		}
+	
+	if(isset($_GET['wineryName']) && $_GET['wineryName']!=""){   
+                array_push($queryInputs, "winery_name = \"".$_GET['wineryName']."\"");
+        	}
+
+	 if($_GET['region'] != "--Select--"){
+		$allRegions = [];
+		foreach($_GET['region'] as $regionOption){
+                	array_push($allRegions, "region_name like \"".$regionOption."%\"");
+                	}
+		$allRegionsText = "(".join($allRegions, " or ").")";
+		array_push($queryInputs, $allRegionsText);	
+		}	
+
+	if($_GET['grape'] != "--Select--"){
+                $allGrapes = [];
+                foreach($_GET['grape'] as $grapeOption){
+                        array_push($allGrapes, "variety like \"".$grapeOption."%\"");
+                        }
+                $allGrapesText = "(".join($allGrapes, " or ").")";
+                array_push($queryInputs, $allGrapesText);
+                }
+
+	if($_GET['from'] != "--Select--"){
+                array_push($queryInputs, "year >= ".$_GET['from']);
+                }
+
+	if($_GET['to'] != "--Select--"){
+                array_push($queryInputs, "year <= ".$_GET['to']);
+                }
+
+	 if($_GET['min'] != ""){
+		array_push($queryInputs, "on_hand >= ".$_GET['min']);
+		}
+
+	if($_GET['max'] != ""){
+                array_push($queryInputs, "on_hand <= ".$_GET['max']);
+                }
+
+
+	//Join all queries into a single query
+
+	$query = $query.join($queryInputs, " and ")." order by wine.wine_id;";
+	echo $query;
+	return $query;
+}
 
 ?>
 
 <script type="text/javascript">
 
-function submitSearch(){
-	var query = "select * from wine where ";
-	//boolean to make sure that at least one field has a value
-	var nonBlankField = 0;
-	var queryEntries = new Array();
+function validateSubmit(){
 
-//Sweep through fields and construct overall query to submit
+var namePatt=/[a-zA-Z0-9]+/;
+var numberPatt=/[0-9]+/;
+var valid = 1;
+var fieldsPopulated = 0;
 
-var wine = document.getElementById("wineName").value;
-if(wine != ""){
-	queryEntries.push("wine_name = " + wine);
+/*Reset all error messages*/
+document.getElementById("wineError").style.display="none";
+document.getElementById("wineryError").style.display="none";
+
+
+var wineNameJs = document.getElementById("wineName").value;
+
+if(wineNameJs.length > 0){
+	fieldsPopulated = 1;
+	if(!namePatt.test(wineNameJs)){
+		document.getElementById("wineError").style.display="inline";
+		valid = 0;
+		}
 	}
 
-query = query + queryEntries.join(" and ") + ";";
+var wineryNameJs = document.getElementById("wineryName").value;
+if(wineryNameJs.length > 0){
+        fieldsPopulated = 1;
+        if(!namePatt.test(wineryNameJs)){
+               	 document.getElementById("wineryError").style.display="inline";
+		 valid = 0;
+                }
+        }
 
-document.getElementById("query").value = query;
+var regionJs = document.getElementById("region");
+var regionSelectJs = regionJs.options[regionJs.selectedIndex].text;
+if(regionSelectJs != "--Select--"){
+        fieldsPopulated = 1;
+        }
 
+var grapeJs = document.getElementById("grape");
+var grapeSelectJs = grapeJs.options[grapeJs.selectedIndex].text;
+if(grapeSelectJs != "--Select--"){
+        fieldsPopulated = 1;
+        }
 
+var fromJs = document.getElementById("from");
+var fromSelectJs = fromJs.options[fromJs.selectedIndex].text;
+if(fromSelectJs != "--Select--"){
+        fieldsPopulated = 1;
+        }
 
-}
+var toJs = document.getElementById("to");
+var toSelectJs = toJs.options[toJs.selectedIndex].text;
+if(toSelectJs != "--Select--"){
+        fieldsPopulated = 1;
+        }
 
-function compileQuery(){
+var minStockJs = document.getElementById("minStock").value;
+if(minStockJs.length > 0){
+        fieldsPopulated = 1;
+        if(!numberPatt.test(minStockJs)){
+                document.getElementById("minStockError").style.display="inline";
+		valid = 0;
+                }
+        }
+
+var maxStockJs = document.getElementById("maxStock").value;
+if(maxStockJs.length > 0){
+        fieldsPopulated = 1;
+        if(!numberPatt.test(maxStockJs)){
+                document.getElementById("maxStockError").style.display="inline";
+		valid = 0;
+                }
+        }
+
+var minPriceJs = document.getElementById("minPrice").value;
+if(minPriceJs.length > 0){
+        fieldsPopulated = 1;
+        if(!numberPatt.test(minPriceJs)){
+                document.getElementById("minPriceError").style.display="inline";
+		valid = 0;
+                }
+        }
+
+var maxPriceJs = document.getElementById("maxPrice").value;
+if(maxPriceJs.length > 0){
+        fieldsPopulated = 1;
+        if(!numberPatt.test(maxPriceJs)){
+                document.getElementById("maxPriceError").style.display="inline";
+		valid = 0;
+                }
+        }
+
+ 
+if(fieldsPopulated == 1){
 	
+	if(valid == 1){
+		document.getElementById("submitted").value = "1";
+		document.getElementById("wineSearch").submit();
+		}
+
 	}
+	else{ 
+		document.getElementById("noValuesError").style.display="inline";
+		}
+}
 
 </script>
